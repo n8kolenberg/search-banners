0371b371031f4c178f3b3ab6245ba0ac625c6ade
Nathaniel Kolenberg <nkolenberg@gmail.com>
2018-08-26 17:17:14 +0400

    Remove reference to v-calendar in main.js

--- src/main.js
+++ src/main.js
@@ -2,19 +2,11 @@ import Vue from 'vue';
 import {DatePicker} from 'element-ui';
 import lang from 'element-ui/lib/locale/lang/en';
 import locale from 'element-ui/lib/locale';
-import vCalendar from 'v-calendar';
-// import 'v-calendar/lib/v-calendar.min.css';
 import App from './App.vue';
-
-//Use v-calendar, v-date-picker & v-popover components
-Vue.use(vCalendar, {
-  firstDayOfWeek: 1
-});
 
 // configure language
 locale.use(lang);
 Vue.use(DatePicker, {locale});
-
 
 
 new Vue({
